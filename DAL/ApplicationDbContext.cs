42cb9e73c4cf66260114fcf564add5cc35c2df26
liashenko.y <yevhen.liashenko@nixs.com>
2023-11-30 11:16:31 +0100

    Added bascu DAL and fixed some remarks

---
+++ DAL/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Transfer> Transfers { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<TransferCondition> TransferConditions { get; set; }
+        public DbSet<Analytic> Analytics { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
