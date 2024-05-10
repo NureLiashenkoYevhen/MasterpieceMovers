942292fa682a8ed494e77ed63a8a689000b3401f
liashenko.y <yevhen.liashenko@nixs.com>
2024-05-10 09:23:06 +0200

    /

--- API/Program.cs
+++ API/Program.cs
@@ -84,11 +84,8 @@ builder.Services.AddScoped<IPasswordService, PasswordService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 //Added new comment
 app.UseHttpsRedirection();
